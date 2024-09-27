7aeb5b8cd9894bcb5a0c0032aa85abd70f451bea
Henrik Knudsen (Viking Food - Bermuda) <henrik@viking.bm>
2024-09-27 10:34:09 -0300

    darkmode

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    // Store user preference in local storage
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.setItem('theme', '');
+    }
+});
+
+// Check if user preference exists in local storage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    body.classList.add(currentTheme);
+}
